ee98c9d8e229236a6eb80a8494ee828240aec656
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-27 15:31:13 -0300

    ARRAY + INDEXOF

--- Ej5.js
+++ Ej5.js
@@ -2,3 +2,7 @@
 Tienes una lista de colores ["Rojo", "Verde", "Azul", "Amarillo"]. 
 Encuentra y muestra el índice del color "Azul" en la lista.
 */
+
+let colores = ["Rojo", "Verde", "Azul", "Amarillo"];
+let indiceAzul = colores.indexOf("Azul");
+console.log("El color AZUL se encuentra en el índice:", indiceAzul); 
