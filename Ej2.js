43d278a1b6bb1b03f29f050ef27cb2d119842dba
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-27 12:54:27 -0300

    ARRAY + POP

--- Ej2.js
+++ Ej2.js
@@ -4,7 +4,7 @@ El último invitado no puede asistir.
 Elimina al último invitado y muestra quién fue eliminado y el estado actualizado de la lista.
 */
 let listaDeInvitados = ["Ana", "Juan", "Carlos", "Sofía"];
-console.log("Lista de invitados:", listaDeInvitados);
+console.log("Lista de invitados:", listaDeInvitados); // Agregado por prolijidad
 let invitadoEliminado = listaDeInvitados.pop();
 console.log("Eliminamos de la lista a:", invitadoEliminado);
 console.log("Lista de invitados actualizada:", listaDeInvitados);
