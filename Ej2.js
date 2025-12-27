239bb1f7838dac0b1e66cae617dffef3daec6858
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-27 12:48:16 -0300

    ARRAY + POP

--- Ej2.js
+++ Ej2.js
@@ -3,4 +3,8 @@ Tienes una lista de invitados a una fiesta ["Ana", "Juan", "Carlos", "Sofía"].
 El último invitado no puede asistir. 
 Elimina al último invitado y muestra quién fue eliminado y el estado actualizado de la lista.
 */
-
+let listaDeInvitados = ["Ana", "Juan", "Carlos", "Sofía"];
+console.log("Lista de invitados:", listaDeInvitados);
+let invitadoEliminado = listaDeInvitados.pop();
+console.log("Eliminamos de la lista a:", invitadoEliminado);
+console.log("Lista de invitados actualizada:", listaDeInvitados);
