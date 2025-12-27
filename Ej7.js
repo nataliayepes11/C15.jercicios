b8ff19cc7410fe7e0bd0ff6502a8203364513f53
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-27 15:55:23 -0300

    ARRAY + INCLUDES + IF/ELSE

--- Ej7.js
+++ Ej7.js
@@ -2,3 +2,12 @@
 Tienes una lista de comidas ["Pizza", "Hamburguesa", "Tacos"]. 
 Verifica si "Sushi" está en la lista y muestra un mensaje que confirme si está o no.
 */
+
+let comidas = ["Pizza", "Hamburguesa", "Tacos"];
+let tieneSushi = comidas.includes("Sushi");       // Si me piediera true o false, lo terminaría acá mostrando solo esta respuesta.
+if (tieneSushi) {                                 // Entiendo que el ejercición pide que confirme SI ESTÁ,
+    console.log("Siii SUSHI está en la lista!!");
+} else {                                          // O si NO ESTÁ, por eso agregué los condicionales.
+    console.log("No, SUSHI no está en la lista.");
+}
+
