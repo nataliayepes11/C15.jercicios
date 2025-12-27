5fd5c198fea9ac2fdf72754e4300fe0dc131c038
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-27 13:26:03 -0300

    ARRAY + UNSHIFT/SHIFT

--- Ej3.js
+++ Ej3.js
@@ -3,3 +3,12 @@ Tienes una fila de mascotas en una clínica veterinaria ["Perro", "Gato", "Conej
 Llega un "Hámster" que debe ser atendido primero. Agrega el "Hámster" al inicio de la lista. 
 Luego, atiende a la primera mascota de la fila y muéstrala. Finalmente, muestra la lista actualizada.
 */
+
+let listaDeMascotas = ["Perro", "Gato", "Conejo"];
+console.log("Lista de mascotas:", listaDeMascotas); //Agregado por prolijidad
+listaDeMascotas.unshift("Hámster");
+console.log("Lista de mascotas:", listaDeMascotas);
+let primeraMascota = listaDeMascotas.shift();
+console.log("Es el turno de:", primeraMascota);
+console.log("Lista de mascotas actualizada:", listaDeMascotas);
+
