39950fd1ab2d2bc0fcb4412b4b40c16e243c1c3e
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-27 15:40:47 -0300

    ARRAY + LASTINDEXOF

--- Ej6.js
+++ Ej6.js
@@ -2,3 +2,9 @@
 Tienes una lista de números [2, 5, 7, 2, 8, 7]. 
 Encuentra y muestra la última posición en la que aparece el número 7.
 */
+
+let numeros = [2, 5, 7, 2, 8, 7];
+let ultimoSiete = numeros.lastIndexOf(7);
+console.log("La última posición en la que aparece el número 7 es:", (ultimoSiete + 1)); // Le sumo 1 para que cualquier mortal 
+// lea posición, aunque nosotras sabemos que es el indice 5 del array)
+
