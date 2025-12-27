73e81bda2f29a5453f4f7094e805e9bd849856c9
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-27 12:36:29 -0300

    ARRAY + PUSH

--- Ej1.js
+++ Ej1.js
@@ -3,3 +3,8 @@ Tienes una lista de personajes de videojuegos ["Link", "Zelda"].
 Agrega los personajes "Mario" y "Luigi" al final de la lista utilizando el método adecuado. 
 Luego, muestra el array actualizado.
 */
+
+let personajesVideojuegos = ["Link", "Zelda"];
+personajesVideojuegos.push("Mario", "Luigi");
+console.log("Ahora los personajes de videojuegos son:", personajesVideojuegos);
+
