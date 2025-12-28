0254d2102a70cb07477aa15c89b797223e928800
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-28 08:09:07 -0300

    STRINGS + MÉTODO SLICE

--- Ej8.js
+++ Ej8.js
@@ -2,3 +2,11 @@
 Tienes la frase "Los gatos son geniales". 
 Extrae y muestra solo la parte "gatos" utilizando el método adecuado.
 */
+
+let frase = "Los gatos son geniales";
+let gatos = frase.slice(4,9);
+console.log("Extraje de la frase la palabra:" , `${gatos}`);
+
+
+
+
