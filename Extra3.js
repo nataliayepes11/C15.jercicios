256b519ecfc3a7437057544a67d9aa83c2a7204b
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-28 11:48:30 -0300

    ARRAY + WHILE + SHIFT + IF

--- Extra3.js
+++ Extra3.js
@@ -1,4 +1,4 @@
-/* ●	Simulador de una cola de atención al cliente
+/* Simulador de una cola de atención al cliente
 Simula una cola de atención al cliente donde los clientes llegan y son atendidos uno por uno. 
 Tienes una lista inicial de clientes en espera y un bucle que los atiende de a uno. Realiza lo siguiente:
 1.	Mientras haya clientes en la cola, atiende a cada uno y muéstralo por consola.
@@ -6,3 +6,17 @@ Tienes una lista inicial de clientes en espera y un bucle que los atiende de a u
 3.	Al final, muestra cuántos clientes fueron atendidos en total.
 Lista inicial de clientes: ["Ana", "Carlos", "VIP", "Sofía", "Juan"].
 */
+
+let clientes = ["Ana", "Carlos", "VIP", "Sofía", "Juan"];
+let contador = 0
+console.log("Lista de clientes:", clientes);                     // Para que quede más prolijo.
+
+while (clientes.length > 0) {
+let cliente1 = clientes.shift();
+console.log("Atendiendo a:", cliente1);
+if (cliente1 === "VIP") {
+    console.log("Atención, el cliente " + cliente1 + " tiene prioridad!");
+}
+contador ++;
+}
+console.log("Total de clientes atendidos:", contador);
