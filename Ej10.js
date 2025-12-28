05567733bf0bc37d267abec498dd2bdc8078330e
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-28 08:22:53 -0300

    STRING + REPLACE

--- Ej10.js
+++ Ej10.js
@@ -2,3 +2,7 @@
 Tienes la frase "Me gusta el invierno". 
 Cambia la palabra "invierno" por "verano" y muestra la frase actualizada.
 */ 
+
+let frase = ("Me gusta el invierno.");
+let palabraReemplazada = frase.replace("invierno","verano");
+console.log("Frase actualizada:" , `${palabraReemplazada}`);
