ed626ebf5e9cb9af720ab64da63d01f33467e570
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-28 08:15:45 -0300

    STRING + TRIM

--- Ej9.js
+++ Ej9.js
@@ -3,3 +3,7 @@ Tienes la siguiente cadena con espacios adicionales
 "    Hola, mundo!    "
 Elimina los espacios al inicio y al final y muestra la cadena resultante.
 */
+
+let cadena = "    Hola, mundo!    "
+let cadenaSinEspacios = cadena.trim();
+console.log("Veamos cómo queda...", `${cadenaSinEspacios}`);
