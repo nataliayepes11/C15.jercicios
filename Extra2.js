602a601574882e46f73eac9015e2aa4569f77272
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-28 09:30:24 -0300

    STRING + SPLIT + ARRAY + CICLO FOR + IF/ELSE + PUSH

--- Extra2.js
+++ Extra2.js
@@ -1,4 +1,4 @@
-/* ●	Clasificación de palabras según su longitud
+/* Clasificación de palabras según su longitud
 Tienes una lista de palabras en una oración. Debes:
 1.	Dividir la oración en palabras individuales.
 2.	Clasificar las palabras en dos categorías:
@@ -7,3 +7,23 @@ Tienes una lista de palabras en una oración. Debes:
 3.	Mostrar ambas listas de palabras clasificadas.
 4.	La oración es: "JavaScript es un lenguaje poderoso y versátil".
 */
+
+let oracion = ("JavaScript es un lenguaje poderoso y versátil");
+let palabrasIndividuales = oracion.split(" ");
+console.log("1) ", palabrasIndividuales);
+//2)
+let palabrasCortas = [];
+let palabrasLargas = [];
+
+for (let i = 0; i < palabrasIndividuales.length; i++) {
+    if (palabrasIndividuales[i].length < 5) {
+        palabrasCortas.push(palabrasIndividuales[i]);
+    } else {
+        palabrasLargas.push(palabrasIndividuales[i]);
+    }
+} 
+console.log("3) Palabras cortas:", palabrasCortas);
+console.log("3) Palabras largas:", palabrasLargas);
+
+
+
