8af211aab7f0e8efd734726fa32e79fc1fa7fb1c
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-28 08:18:03 -0300

    STRING + SLICE

--- Ej8.js
+++ Ej8.js
@@ -1,7 +1,6 @@
-/* Ejercicio 8: Extraer una parte de una frase
-Tienes la frase "Los gatos son geniales". 
-Extrae y muestra solo la parte "gatos" utilizando el método adecuado.
-*/
+// Ejercicio 8: Extraer una parte de una frase
+// Tienes la frase "Los gatos son geniales". 
+// Extrae y muestra solo la parte "gatos" utilizando el método adecuado.
 
 let frase = "Los gatos son geniales";
 let gatos = frase.slice(4,9);
