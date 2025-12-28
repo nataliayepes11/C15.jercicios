eeaee751d3fcffd7bced3f6803030483de6cb881
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-28 08:50:15 -0300

    ARRAY + PUSH + INCLUDES + IF/ELSE + SHIFT + JOIN

--- Extra1.js
+++ Extra1.js
@@ -1,4 +1,4 @@
-/* ●	Gestión de inventario de una tienda
+/* Gestión de inventario de una tienda
 Eres encargado de una tienda y tienes un inventario inicial de productos. La lista de productos es:
 let inventario = ["Laptop", "Teclado", "Mouse", "Monitor"];
 Debes realizar las siguientes acciones:
@@ -7,3 +7,18 @@ Debes realizar las siguientes acciones:
 3.	Eliminar el primer producto del inventario.
 4.	Mostrar la lista actualizada de productos en una sola cadena, separados por comas.
 */
+
+let inventario = ["Laptop", "Teclado", "Mouse", "Monitor"];
+console.log("Inventario inicial:", inventario);                  // Para que quede más bonito en la terminal
+inventario.push("Impresora");
+console.log("Inventario actualizado:", inventario);              // Para que quede más bonito en la terminal
+let hayTablet = inventario.includes("Tablet");
+if (hayTablet) {                                                 // Para que quede más bonito en la terminal
+    console.log("Si, tenemos en el inventario TABLET");
+} else {
+    console.log("No tenemos en el inventario TABLET");
+}
+let primerProducto = inventario.shift();
+console.log("Inventario modificado:", inventario);               // Para que quede más bonito en la terminal
+let inventarioFinal = inventario.join(`, `);
+console.log("Inventario final:" , inventarioFinal);
